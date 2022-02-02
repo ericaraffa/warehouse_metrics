c37cc0b7f965ac0c03696922708f92061b0ba06f
Giovanni <sentius96@gmail.com>
2022-02-02 20:38:15 +0100

    Created Classes for Product, Review, User

--- WarehouseMetrics/src/Main.java
+++ WarehouseMetrics/src/Main.java
@@ -1,6 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        MongoManager mongo = new MongoManager();
+        int command;
+
+        while (true) {
+            try {
+                showMenu();
+                command = Integer.parseInt(br.readLine());
+                switch (command) {
+
+                    // Browse products
+                    case 1 :
+                        mongo.browseProducts();
+                        break;
+
+                    // Browse products
+                    case 2 :
+                        mongo.browseCategories();
+                        break;
+
+                    // Browse products with price filter
+                    case 3 :
+                        mongo.browseProductsByPrice(br);
+                        break;
+
+                    // Browse products with category filter
+                    case 4 :
+                        mongo.browseProductsByCategory(br);
+                        break;
+
+                    // Exit
+                    case 0 :
+                        return;
+
+                    // Invalid input
+                    default :
+                        break;
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void showMenu() {
+        System.out.println("\nSelect an operation: ");
+        System.out.println("1) Browse products ");
+        System.out.println("2) Browse categories ");
+        System.out.println("3) Browse products with price filter ");
+        System.out.println("4) Browse products with category filter ");
+        System.out.println("0) Exit application ");
     }
 }
--- WarehouseMetrics/src/MongoManager.java
+++ WarehouseMetrics/src/MongoManager.java
@@ -1,29 +1,105 @@
 import com.mongodb.ConnectionString;
 import com.mongodb.client.*;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.function.Consumer;
+
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Sorts.*;
 
 public class MongoManager {
-    public static void main(String[] args) {
 
+    // Browse products
+    public void browseProducts() {
         ConnectionString uri = new ConnectionString("mongodb://localhost:27017");
         MongoClient myClient = MongoClients.create(uri);
         MongoDatabase conn_db = myClient.getDatabase("warehouse_metrics");
         MongoCollection<Document> collection = conn_db.getCollection("Products");
 
-        Document doc = new Document("name", "MongoDB")
-                .append("count", 1)
-                .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
-                .append("info", new Document("x", 203).append("y", 102));
+        Consumer<Document> printDocuments =  doc -> System.out.println(doc.toJson());
+        collection.find().forEach(printDocuments);
+        myClient.close();
+    }
 
-        //collection.insertOne(doc);
+    // Browse categories
+    public void browseCategories() {
+        ConnectionString uri = new ConnectionString("mongodb://localhost:27017");
+        MongoClient myClient = MongoClients.create(uri);
+        MongoDatabase conn_db = myClient.getDatabase("warehouse_metrics");
+        MongoCollection<Document> collection = conn_db.getCollection("Products");
 
-        MongoCursor<Document> cursor = collection.find().iterator();
-        {
-            while (cursor.hasNext()) {
-                System.out.println(cursor.next().toJson());
+        Consumer<Document> printDocuments =  doc -> System.out.println(doc.toJson());
+
+        Bson groupCategories = group("$categories");
+        Bson sortCategories = sort(ascending("_id"));
+        Bson projectCategories = project(fields(computed("categories","$_id")));
+        collection.aggregate(Arrays.asList(groupCategories, sortCategories, projectCategories)).forEach(printDocuments);
+    }
+
+    // Browse products with price filter
+    public void browseProductsByPrice(BufferedReader br) {
+        ConnectionString uri = new ConnectionString("mongodb://localhost:27017");
+        MongoClient myClient = MongoClients.create(uri);
+        MongoDatabase conn_db = myClient.getDatabase("warehouse_metrics");
+        MongoCollection<Document> collection = conn_db.getCollection("Products");
+
+        Consumer<Document> printDocuments =  doc -> System.out.println(doc.toJson());
+
+        try {
+            System.out.println("Insert the lower bound");
+            int lowerPrice = Integer.parseInt(br.readLine());
+            System.out.println("Insert the upper bound");
+            int upperPrice = Integer.parseInt(br.readLine());
+
+            collection.find(and(gt("price", lowerPrice), lte("price", upperPrice))).forEach(printDocuments);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (myClient != null)
+                    myClient.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
     }
+
+    // Browse products with category filter
+    public void browseProductsByCategory(BufferedReader br) {
+        ConnectionString uri = new ConnectionString("mongodb://localhost:27017");
+        MongoClient myClient = MongoClients.create(uri);
+        MongoDatabase conn_db = myClient.getDatabase("warehouse_metrics");
+        MongoCollection<Document> collection = conn_db.getCollection("Products");
+
+        Consumer<Document> printDocuments =  doc -> System.out.println(doc.toJson());
+
+        Bson groupCategories = group("$categories");
+        Bson sortCategories = sort(ascending("categories"));
+        Bson projectCategories = project(fields(excludeId(), include("categories")));
+        collection.aggregate(Arrays.asList(sortCategories, groupCategories)).forEach(printDocuments);
+
+        System.out.println("Insert a category");
+        try {
+            String category = br.readLine();
+            Bson matchCategories = match(eq("categories", category));
+            collection.aggregate(Arrays.asList(matchCategories)).forEach(printDocuments);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (myClient != null)
+                    myClient.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
 }
